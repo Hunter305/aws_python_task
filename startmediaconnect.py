7c42097aa96785665c308492ccf7773dabda3aa7
Hunter305 <avinash8169@gmail.com>
2024-03-11 20:29:14 +0530

    completed media connect

--- startmediaconnect.py
+++ startmediaconnect.py
@@ -3,13 +3,13 @@ import boto3
 mediaconnect_client = boto3.client("mediaconnect")
 
 
-def stop_mediaconnect_flow(arn):
-    response = mediaconnect_client.stop_flow(
+def start_mediaconnect_flow(arn):
+    response = mediaconnect_client.start_flow(
         FlowArn=arn
     )
-    print("service stopped")
+    print("service started")
 
 
 arn = 'arn:aws:mediaconnect:us-east-1:891377081681:flow:1-DV0NWwIHBQcHUVgF-9f22481af7fb:amagi_test'
 
-stop_mediaconnect_flow(arn)
+start_mediaconnect_flow(arn)
