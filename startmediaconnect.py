26410ab0c59a65e30a2ab0bbad9ed64471bd7b24
Hunter305 <avinash8169@gmail.com>
2024-03-12 20:38:44 +0530

    hadled mediaconnect using exception handling

--- startmediaconnect.py
+++ startmediaconnect.py
@@ -4,10 +4,18 @@ mediaconnect_client = boto3.client("mediaconnect")
 
 
 def start_mediaconnect_flow(arn):
-    mediaconnect_client.start_flow(
-        FlowArn=arn
-    )
-    print("service started")
+    try:
+        response = mediaconnect_client.describe_flow(FlowArn=arn)
+        status = response['Flow']['Status']
+
+        if status == 'ACTIVE':
+            print("MediaConnect flow is already running.")
+        else:
+            mediaconnect_client.start_flow(FlowArn=arn)
+            print("Service started.")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 
 arn = 'arn:aws:mediaconnect:us-east-1:891377081681:flow:1-DV0NWwIHBQcHUVgF-9f22481af7fb:amagi_test'
--- stopmediaconnect.py
+++ stopmediaconnect.py
@@ -4,10 +4,18 @@ mediaconnect_client = boto3.client("mediaconnect")
 
 
 def stop_mediaconnect_flow(arn):
-    response = mediaconnect_client.stop_flow(
-        FlowArn=arn
-    )
-    print("service stopped")
+    try:
+        response = mediaconnect_client.describe_flow(FlowArn=arn)
+        status = response['Flow']['Status']
+
+        if status == 'STANDBY':
+            print("MediaConnect flow is already in standby.")
+        else:
+            mediaconnect_client.start_flow(FlowArn=arn)
+            print("Service stopped.")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 
 arn = 'arn:aws:mediaconnect:us-east-1:891377081681:flow:1-DV0NWwIHBQcHUVgF-9f22481af7fb:amagi_test'
